3ffa94aaea6aa8990abd99eeb0956b04d8085435
ljw <ljw@e-no.tv>
2022-10-20 11:40:35 +0900

    vite config 에 host, port를 지정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+const env = loadEnv('', process.cwd(), '');
+
 export default defineConfig({
+    server: {
+        host: env.SERVER_IP,
+        port: env.VITE_PORT,
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
